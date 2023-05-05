bc4ba95f62f6cee2da2a7478dc267f92e706892c
Selim Kaan Baykara <selim.k.baykara98@outlook.de>
2023-05-05 18:10:19 +0200

    uncheck dist

--- streaming-service/dist/app.controller.d.ts
+++
@@ -1,8 +0,0 @@
-import { AppService } from './app.service';
-import { Request, Response } from 'express';
-export declare class AppController {
-    private readonly appService;
-    constructor(appService: AppService);
-    getHello(): string;
-    getTestVideo(req: Request, res: Response): void;
-}
